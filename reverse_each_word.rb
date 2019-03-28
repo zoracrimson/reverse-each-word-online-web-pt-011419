f854679439c8dde945213dbf55059e188386c6e2
Kat Maldon <katharinamaldon@gmail.com>
2019-03-28 17:01:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 
 
-
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
-end
+def reverse_each_word(string)
+  string.split.collect do | word |
+    word.reverse.join(" ")
+  end 
+end 
